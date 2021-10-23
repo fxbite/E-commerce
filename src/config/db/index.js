8687cc4c5fc07299ba9241d3a6f1d81dd5c088c4
Fxbite <letruongthinh1703@gmail.com>
2021-10-23 10:35:44 +0700

    Add models and frontend

---
+++ src/config/db/index.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+
+async function connect() {
+    
+    const URI = 'mongodb+srv://admin:XMnuIU05kyjV2x7r@e-commerce.asumm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+
+    try {
+        await mongoose.connect(URI, {useNewUrlParser: true, useUnifiedTopology: true});
+        console.log(`DB connected successfully`)
+    } catch (error) {
+        console.log(`Connect failed !!! `)
+    }
+    
+
+}
+
+module.exports = { connect };
--- src/index.js
+++ src/index.js
@@ -7,8 +7,13 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 3000;
 
-// HTTP Logger
-app.use(morgan('dev'));
+// Connect to DB
+const db = require('./config/db');
+db.connect();
+
+// Route init
+const route = require('./routes')
+route(app);
 
 // Static public file
 app.use(express.static(path.join(__dirname, 'public')));
@@ -18,9 +23,8 @@ app.engine('.hbs', exphbs({extname: '.hbs'}));
 app.set('view engine', '.hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
-// Route init
-const route = require('./routes')
-route(app);
+// HTTP Logger
+app.use(morgan('dev'));
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
